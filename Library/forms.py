2e960315e3f4f0abdaaaf356b4f3667ef5aa3c4f
kiminzajnr <kiminzaeric@gmail.com>
2024-09-12 15:52:51 +0300

    Add WTForm for adding a book

---
+++ Library/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, StringField, SubmitField, FloatField
+from wtforms.validators import InputRequired, NumberRange
+
+
+class BooksForm(FlaskForm):
+    title = StringField("Title", validators=[InputRequired()])
+    Author = StringField("Author", validators=[InputRequired()])
+
+    quantity = IntegerField("Quantity", validators=[InputRequired(), NumberRange(min=1, message="Quantity must be atleast 1")])
+
+    fee = FloatField("Fee", validators=[InputRequired(), NumberRange(min=0, message="Fee must be a positive value")])
+
+    submit = SubmitField("Add Book")
