7e5088defa0c918a16ff1b4fc1eac6bfb130b7c0
kiminzajnr <kiminzaeric@gmail.com>
2024-09-13 14:29:46 +0300

    Add render wtform

--- Library/forms.py
+++ Library/forms.py
@@ -5,10 +5,10 @@ from wtforms.validators import InputRequired, NumberRange
 
 class BooksForm(FlaskForm):
     title = StringField("Title", validators=[InputRequired()])
-    Author = StringField("Author", validators=[InputRequired()])
+    author = StringField("Author", validators=[InputRequired()])
 
     quantity = IntegerField("Quantity", validators=[InputRequired(), NumberRange(min=1, message="Quantity must be atleast 1")])
 
-    fee = FloatField("Fee", validators=[InputRequired(), NumberRange(min=0, message="Fee must be a positive value")])
+    fee = FloatField("Issue Fee", validators=[InputRequired(), NumberRange(min=0, message="Fee must be a positive value")])
 
     submit = SubmitField("Add Book")
