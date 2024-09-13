6868b3b61c359601156acfc8403da957f18637d7
kiminzajnr <kiminzaeric@gmail.com>
2024-09-13 15:59:00 +0300

    Feat: Save to SQLite DB

--- Library/routes.py
+++ Library/routes.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, url_for, redirect
 from Library.forms import BooksForm
+from Library.db import db
+from Library.models import BookModel
 
 
 pages = Blueprint(
@@ -15,12 +17,27 @@ def index():
     )
 
 
-@pages.route("/add", methods=["GET", "POST"])
+@pages.route("/add_book", methods=["GET", "POST"])
 def add_book():
     form = BooksForm()
 
     if form.validate_on_submit():
-        pass
+        title = form.title.data
+        author = form.author.data
+        quantity = form.quantity.data
+        fee = form.fee.data
+
+        new_book = BookModel(
+            title=title,
+            author=author,
+            quantity=quantity,
+            fee=fee
+        )
+
+        db.session.add(new_book)
+        db.session.commit()
+
+        return redirect(url_for(".index"))
 
     return render_template(
         "new_book.html", title="Library Management App - Add Book", form=form
