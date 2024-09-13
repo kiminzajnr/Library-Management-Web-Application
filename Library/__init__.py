3e81ec0c5816cd91fbde82fc4183b15d4a8e43b8
kiminzajnr <kiminzaeric@gmail.com>
2024-09-13 14:29:46 +0300

    Add render wtform

--- Library/__init__.py
+++ Library/__init__.py
@@ -9,6 +9,9 @@ load_dotenv()
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = os.environ.get(
+        "SECRET_KEY", "pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFw"
+    )
 
     app.register_blueprint(pages)
 
