b9f7b8ec11a06934eea734cd886013d4a22d3c3b
kiminzajnr <kiminzaeric@gmail.com>
2024-09-13 15:58:29 +0300

    Add init for easy imports

--- Library/__init__.py
+++ Library/__init__.py
@@ -1,6 +1,9 @@
 import os
+import Library.models
 from flask import Flask
 from dotenv import load_dotenv
+
+from Library.db import db
 
 from Library.routes import pages
 
@@ -12,6 +15,13 @@ def create_app(db_url=None):
     app.config["SECRET_KEY"] = os.environ.get(
         "SECRET_KEY", "pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFw"
     )
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
 
     app.register_blueprint(pages)
 
