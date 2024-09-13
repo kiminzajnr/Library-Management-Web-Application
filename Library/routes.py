c83be90667a40423d0f38cd88ad48b2092765dc7
kiminzajnr <kiminzaeric@gmail.com>
2024-09-13 14:29:46 +0300

    Add render wtform

--- Library/routes.py
+++ Library/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from Library.forms import BooksForm
 
 
 pages = Blueprint(
@@ -11,4 +12,16 @@ def index():
     return render_template(
         "index.html",
         title="Library Management Web Application",
+    )
+
+
+@pages.route("/add", methods=["GET", "POST"])
+def add_book():
+    form = BooksForm()
+
+    if form.validate_on_submit():
+        pass
+
+    return render_template(
+        "new_book.html", title="Library Management App - Add Book", form=form
     )
