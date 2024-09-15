003db63d1e15d75ce8c04332901901632f410439
kiminzajnr <kiminzaeric@gmail.com>
2024-09-15 16:02:29 +0300

    Add feature to issue a book to a member and manage transactions

---
+++ Library/models/transactions.py
@@ -0,0 +1,16 @@
+import datetime
+from Library.db import db
+
+
+class TransactionModel(db.Model):
+    __tablename__ = "transactions"
+
+    id = db.Column(db.Integer, primary_key=True)
+    book_id = db.Column(db.Integer, db.ForeignKey("books.id"), nullable=False)
+    member_id = db.Column(db.Integer, db.ForeignKey("members.id"), nullable=False)
+    borrow_date = db.Column(db.DateTime, default=datetime.datetime.today())
+    return_date = db.Column(db.DateTime, nullable=True)
+
+    book = db.relationship("BookModel", backref="transactions")
+    member = db.relationship("MemberModel", backref="transactions")
+
