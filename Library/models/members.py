5fbc48e892230fdde39782b48d8098d1c85cccb8
kiminzajnr <kiminzaeric@gmail.com>
2024-09-16 00:16:36 +0300

    Fix delete integrity error

--- Library/models/members.py
+++ Library/models/members.py
@@ -6,5 +6,5 @@ class MemberModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String, unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
     debt = db.Column(db.Float, default=0)
